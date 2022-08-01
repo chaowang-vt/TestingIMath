93049f75d1c3cad9c6c6a8599497217554a49930
chaowang <wang626@usc.edu>
2022-07-31 22:02:17 -0700

    add the app activity and layout files

--- app/src/main/java/com/example/testingimath/MainActivity.java
+++ app/src/main/java/com/example/testingimath/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.testingimath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickCalculate(View view){
+        EditText inputView = (EditText) findViewById(R.id.editTextNumber);
+        TextView outputView = (TextView) findViewById(R.id.textView);
+
+        IMath im = new IMath();
+        int input = Integer.valueOf(inputView.getText().toString());
+        int output = im.isqrt(input);
+        outputView.setText(String.valueOf(output));
     }
 }
